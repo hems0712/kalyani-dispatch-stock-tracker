438188102ea86ff01541bbb01cc6a4a5aa5915ed
hems0712 <hemalb0712@gmail.com>
2026-06-05 12:08:40 +0530

    Delete src directory

--- src/ai/dev.ts
+++
@@ -1,4 +0,0 @@
-import { config } from 'dotenv';
-config();
-
-import '@/ai/flows/intelligent-shortage-forecast.ts';
--- src/firebase/config.ts
+++
@@ -1,9 +0,0 @@
-
-export const firebaseConfig = {
-  apiKey: "AIzaSyAs-placeholder-key-for-studio",
-  authDomain: "studio-810601601.firebaseapp.com",
-  projectId: "studio-810601601",
-  storageBucket: "studio-810601601.appspot.com",
-  messagingSenderId: "1234567890",
-  appId: "1:1234567890:web:abcdef1234567890"
-};
