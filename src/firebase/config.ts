8da92fd1f9b4e418fdf2e65ea65a1b86174db3a3
hems0712 <hemalb0712@gmail.com>
2026-06-08 14:54:33 +0530

    Delete src directory

--- src/firebase/config.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY ?? "AIzaSyC8H_JsjljNMnNVnyZC6FNlq0elvBCC1SQ",
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN ?? "studio-810601601-bec50.firebaseapp.com",
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID ?? "studio-810601601-bec50",
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET ?? "studio-810601601-bec50.firebasestorage.app",
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID ?? "442154673134",
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID ?? "1:442154673134:web:4960e7d5124056459ba4ff"
-};
